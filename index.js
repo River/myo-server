835f1e0a78f8f8dcee23153c72f0d5d8aefeadce
River Jiang <rvrjng@gmail.com>
2015-01-17 23:35:22 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/public'));
+//
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'));
+// });
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+var http = require('http');
+var fs = require('fs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+var stateFile = 'state'
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
+http.createServer(function (req, res) {
+  url = req.url;
+
+  if (url == '/read') {
+    fs.readFile(stateFile, function(err, data) {
+      fs.writeFile(stateFile, '0');
+      res.end(data);
+    });
+  } else if (url.substring(0,6) == '/write') {
+    var writeString = url.substring(7);
+    fs.writeFile(stateFile, writeString);
+    res.end(writeString);
+  } else {
+    res.end('Not found');
+  }
+
+}).listen(process.env.PORT || 5000);
